4e39cb09e695a6d56ff419c3ae2d13ffcc65952f
Gray <grayzprogrammer@gmail.com>
2022-09-23 15:08:00 -0500

    Initial Commit

---
+++ Currencify/CurrencifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencifyApp.swift
+//  Currencify
+//
+//  Created by G Zhen on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct CurrencifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
